cc733d33af05d9b508b546f59b7850882befaf8a
Joseph-N <josendungu@hotmail.com>
2014-02-26 18:08:52 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Downloadrare::Application.config.secret_key_base = 'b95a1de58fa5c9c2040ee16d7a7368b22e9225f05c14bc44e87a0864cc6488708e9c1f5f3dbc30b7f14d2c02aa0ad6534304be68a464be1523b6ca67f4d01b9b'
