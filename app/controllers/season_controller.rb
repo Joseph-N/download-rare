0573c27abc68c15628d6c691261328bb88ce7765
Joseph-N <josendungu@hotmail.com>
2014-03-06 11:40:01 +0300

    fix for empty season overview description

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -3,6 +3,7 @@ class SeasonController < ApplicationController
   	@tv_show = TvShow.friendly.find(params[:tv_show_id])
   	@season = @tv_show.seasons.where("season_number = ?", params[:id]).first
   	@season_info = @tmdb_tv.find_season(@tv_show.tmdb_id, @season.season_number)
+  	@tv_show_info =@tmdb_tv.find(@tv_show.tmdb_id)
   	@episode = Episode.new
   end
 end
