643ca1977fc818c1757cea2858492be87ad4d1f1
Joseph-N <josendungu@hotmail.com>
2014-03-08 00:34:30 +0300

    temporal fix for maximum download count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@featured = Movie.reorder("download_count DESC").first
+  	@featured = Movie.where("download_count = ?", Movie.maximum(:download_count)).first
   	@movies = Movie.limit(9)
   	@shows = TvShow.limit(9)
   	@recently_updated_episodes = Episode.limit(10)
