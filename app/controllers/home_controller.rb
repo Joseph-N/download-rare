6833f14e4e8d7175b60a0dc3c3c23356b8a8b8d9
Joseph-N <josendungu@hotmail.com>
2014-03-08 00:07:14 +0300

    added featured movie as most downloaded movie

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@featured = Movie.all.sample
+  	@featured = Movie.unscoped.order("download_count DESC").first
   	@movies = Movie.limit(9)
   	@shows = TvShow.limit(9)
   	@recently_updated_episodes = Episode.limit(10)
