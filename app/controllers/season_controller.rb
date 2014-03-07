9294361cf81c001293735a8a36db385a90d81d73
Joseph-N <josendungu@hotmail.com>
2014-03-07 17:57:54 +0300

    fixed error when api updates

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -3,7 +3,6 @@ class SeasonController < ApplicationController
   	@tv_show = TvShow.friendly.find(params[:tv_show_id])
   	@season = @tv_show.seasons.where("season_number = ?", params[:id]).first
   	@season_info = @tmdb_tv.find_season(@tv_show.tmdb_id, @season.season_number)
-  	@tv_show_info =@tmdb_tv.find(@tv_show.tmdb_id)
-  	@episode = Episode.new
+  	@tv_show_info = @tmdb_tv.find(@tv_show.tmdb_id)
   end
 end
