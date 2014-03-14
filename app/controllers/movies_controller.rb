eb78403711a0f0f4f73ed860a0042b11a4c38353
Joseph-N <josendungu@hotmail.com>
2014-03-15 00:27:01 +0300

    changed movies to show on search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,9 @@ class MoviesController < ApplicationController
   def index
       # @movies = Movie.plain_tsearch(params[:query]).paginate(:page => params[:page], :per_page => 12) 
       if params[:genre]
-        @movies = Movie.where("? = ANY (genres)", params[:genre]).paginate(:page => params[:page], :per_page => 12)
+        @movies = Movie.where("? = ANY (genres)", params[:genre]).paginate(:page => params[:page], :per_page => 24)
       else
-        @movies = Movie.paginate(:page => params[:page], :per_page => 12)
+        @movies = Movie.paginate(:page => params[:page], :per_page => 24)
       end
       @genres = fetch_genres.flatten.uniq
 
