0e842c8440668358f9b56ff20ea0f3d5e5a2d7a3
Joseph-N <josendungu@hotmail.com>
2014-03-23 01:15:18 +0300

    bug fixes

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -9,7 +9,7 @@ class SeasonController < ApplicationController
 
   def update
   	@tv_show = TvShow.friendly.find(params[:tv_show_id])
-  	@season = @tv_show.seasons.where("season_number = ?", params[:id]).first
+  	@season = @tv_show.seasons.find(params[:id])
   	@season.update_attributes(season_params)
   	CrawlerWorker.perform_async(@season.id, params[:season][:base_url])
   	redirect_to tv_show_season_path(@tv_show, @season), notice: "success"
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 every 1.day do
 	rake "shows:update", :output => { :standard => 'shows-update.log' }
-	command "backup perform -t downloadrare_db", , :output => { :standard => 'db_backup.log' }
+	command "backup perform -t downloadrare_db", :output => { :standard => 'db_backup.log' }
 end
 
 every 2.days do
