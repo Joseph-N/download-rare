f370bb4baa679219fd8176e8980fe826f93dac46
Joseph-N <josendungu@hotmail.com>
2014-03-23 11:16:12 +0000

    fixes and changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,18 +1,18 @@
 every 1.day do
 	rake "shows:update", :output => { :standard => 'shows-update.log' }
-	command "backup perform -t downloadrare_db", :output => { :standard => 'db_backup.log' }
+	command "backup perform -t downloadrare_db", :output => { :standard => '/home/Jose/download-rare/log/db_backup.log' }
 end
 
 every 2.days do
-  rake "fetch:all_broken", :output => { :standard => 'broken_links.log' }
+  rake "fetch:all_broken", :output => { :standard => '/home/Jose/download-rare/log/broken_links.log' }
 end
 
-every 3.days do
-	rake "rake episodes:fetch_links", :output => { :standard => 'episode_links.log' }
+every 1.day, :at => '8am' do
+	rake "rake episodes:fetch_links", :output => { :standard => '/home/Jose/download-rare/log/episode_links.log' }
 end
 
 every :friday, :at => '12pm' do
-	rake "movies:hunt", :output => { :standard => 'movie_hunter.log' }
+	rake "movies:hunt", :output => { :standard => '/home/Jose/download-rare/log/movie_hunter.log' }
 end
 
 
