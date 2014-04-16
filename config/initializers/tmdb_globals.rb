8ab695a2016c9309740744edb689d860f7747ab5
Joseph-N <josendungu@hotmail.com>
2014-04-16 16:52:16 +0300

    checking if this solves production error

--- config/initializers/tmdb_globals.rb
+++ config/initializers/tmdb_globals.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../../lib/assets/tmdb', __FILE__)
-require File.expand_path('../../../lib/assets/tmdb_tv', __FILE__)
-require File.expand_path('../../../lib/assets/tmdb_movie', __FILE__)
-require File.expand_path('../../../lib/assets/yts', __FILE__)
+# require File.expand_path('../../../lib/assets/tmdb', __FILE__)
+# require File.expand_path('../../../lib/assets/tmdb_tv', __FILE__)
+# require File.expand_path('../../../lib/assets/tmdb_movie', __FILE__)
+# require File.expand_path('../../../lib/assets/yts', __FILE__)
