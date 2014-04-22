4c5e5bd38346a46c8ea9010eda7c1307e1e3c407
Joseph-N <josendungu@hotmail.com>
2014-04-22 23:10:03 +0300

    styled search results page

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,10 +4,10 @@ module MoviesHelper
 	end
 
 	def options_for_rating
-		(1...10).to_a.map {|x| x.to_s + '+' }
+		(1...10).to_a.map {|x| [x.to_s + '+', x.to_f] }
 	end
 
 	def options_for_year
-		Movie.select("release_date").map{ |movie| movie.release_date.year }.uniq
+		Movie.select("release_date").map{ |movie| movie.release_date.year }.uniq.sort.reverse
 	end
 end
