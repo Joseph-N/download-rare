04726365cda02d11b153bf52e2c060d32af58025
Joseph-N <josendungu@hotmail.com>
2014-05-07 13:23:30 +0300

    show where download is from

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
 	def index
-		@featured = Movie.where("download_count = ?", Movie.maximum(:download_count)).first
+		@featured = Movie.where("imdb_rating >= ?", 5).sample
 		@movies = Movie.limit(9)
 		@shows = TvShow.limit(9)
 		@recently_updated_episodes = Episode.limit(10)
