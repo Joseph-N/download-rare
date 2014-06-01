78abe1b612a9240d89744538e4124d2f659d211f
Joseph-N <josendungu@hotmail.com>
2014-06-01 16:56:55 +0300

    restrict saving of non-media links

--- app/workers/crawler_worker.rb
+++ app/workers/crawler_worker.rb
@@ -6,7 +6,7 @@ class CrawlerWorker
   
   def perform(season_id, url)
     wait = [1,2,3,4]
-    blacklist = ["php","jpg","png"]
+    blacklist = ["php","jpg","png","html"]
 
     doc = Nokogiri::HTML(open(url))
 
@@ -15,26 +15,35 @@ class CrawlerWorker
       base_url = "http://dl.popbrap.org"
 
       doc.css("br+ a").each_with_index do |entry, index|
-        full_url = base_url + entry["href"]
-        episode_number = index +=1
-        SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        format = entry["href"].split('.').last
+        unless blacklist.include?(format)
+          full_url = base_url + entry["href"]
+          episode_number = index +=1
+          SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        end
       end
 
     # for http://192.0.135.29/ parent directory
     elsif doc.css("tr+ tr a").size > 0
 
       doc.css("tr+ tr a").each_with_index do |entry, index|
-        full_url = url + entry["href"]
-        episode_number = index +=1
-        SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        format = entry["href"].split('.').last
+        unless blacklist.include?(format)
+          full_url = url + entry["href"]
+          episode_number = index +=1
+          SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        end
       end      
 
     # for http://dl1.m-dl.in/
     elsif doc.css("a+ a").size > 0      
       doc.css("a+ a").each_with_index do |entry, index|
-        full_url = url + entry["href"]
-        episode_number = index +=1
-        SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        format = entry["href"].split('.').last
+        unless blacklist.include?(format)
+          full_url = url + entry["href"]
+          episode_number = index +=1
+          SeasonsWorker.perform_in(wait.sample.minutes, season_id, episode_number, full_url)
+        end
       end
 
     # for http://dl1.m-dl.in/ other differently formated pages
