ca90de2eb414c549a9d16b22d8d31f941ec3361b
Joseph-N <josendungu@hotmail.com>
2014-07-30 00:15:47 +0300

    disabled mailing for downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -5,13 +5,13 @@ class DownloadsController < ApplicationController
 			@movie.increment!(:download_count)	
 
 			if params[:type] == "magnetic_link"
-				DownloadMailer.notify("movie", @movie, "magnetic_link").deliver
+				#DownloadMailer.notify("movie", @movie, "magnetic_link").deliver
 				redirect_to @movie.magnetic_link
 			elsif params[:type] == "torrent_file"
-				DownloadMailer.notify("movie", @movie, "torrent_file").deliver
+				#DownloadMailer.notify("movie", @movie, "torrent_file").deliver
 				redirect_to @movie.torrent_file_link
 			else
-				DownloadMailer.notify("movie", @movie, "Direct").deliver
+				#DownloadMailer.notify("movie", @movie, "Direct").deliver
 				redirect_to @movie.download_link
 			end	
 					
@@ -20,7 +20,7 @@ class DownloadsController < ApplicationController
 			@download_link = DownloadLink.find(params[:link_id])
 			@episode.increment!(:download_count)
 
-			DownloadMailer.notify("episode", @episode, "Direct").deliver
+			#DownloadMailer.notify("episode", @episode, "Direct").deliver
 
 			
 			redirect_to @download_link.url
